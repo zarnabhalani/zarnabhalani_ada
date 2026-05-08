b1b0e3f8ba6ffe41027c8d086fbc98af0f468d60
zarnabhalani <zarna0806@gmail.com>
2026-05-08 13:58:19 +0530

    Create Practical13.java

---
+++ Practical13.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class SCS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String s1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String s2 = sc.nextLine();
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0)
+                    dp[i][j] = j;
+                else if (j == 0)
+                    dp[i][j] = i;
+                else if (s1.charAt(i - 1) == s2.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+
+        System.out.println("Length of SCS = " + dp[m][n]);
+    }
+}
